5a6445bed78ee5d1c610ad8695f23400f6825f83
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-11-07 14:37:03 -0700

    First commit

---
+++ MLP_Classification_MNIST.py
@@ -0,0 +1,8 @@
+# Implementation of CNNs -> MNIST Multi-Layer Perceptron tutorial from Machine Learning Engineering Nanodegree - Udacity -> Input by Ryan L Buchanan 07NOV20
+
+# Train an MLP to classify images from the [MNIST database](http://yann.lecun.com/exdb/mnist/) hand-written digit database.
+
+# import libraries
+import torch
+import numpy as np
+
